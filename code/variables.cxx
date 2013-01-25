b4d0445e87fbd8928b83a7a9900c2fbfb631010a
Nakul <gnukool@gmail.com>
2013-01-25 18:14:24 -0500

    Changes to flags, added terminal propagation

---
+++ code/variables.cxx
@@ -0,0 +1,11 @@
+/* Header file for global variables */
+/*********************************************************************
+INTEGER GLOBAL VARIABLES
+********************************************************************/
+unsigned int stepDepth;
+
+/*********************************************************************
+BOOLEAN GLOBAL VARIABLES
+********************************************************************/
+bool debug=false;
+bool performNetHidingConsistency=false;
