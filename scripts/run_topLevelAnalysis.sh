291c55c353955255a488806a48cf34e22b79ef19
nakultirumalai <gnukool@gmail.com>
2013-08-20 18:45:02 -0400

    blah

--- scripts/run_topLevelAnalysis.sh
+++ scripts/run_topLevelAnalysis.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-export BENCHMARK_ROOT=$HOME/Downloads/Benchmarks
+export BENCHMARK_ROOT=$HOME/results
 export SCRIPT_ROOT=$HOME/thesis/VLSIPlacement/scripts
+export UTILS_ROOT=$HOME/Downloads/Benchmarks
 export EXEC_FILE=$HOME/thesis/VLSIPlacement/code/the_exec
 export OUTPUT_PATH=$(pwd)
-export DESIGNS="usb_sie sap"
+export DESIGNS="seq_align reedsoldec"
 export SYNTHESIS_PATH=/opt/CAD/Synopsys/Current/syn/bin
 export ICC_PATH=/opt/CAD/Synopsys/Current/icc/bin
 export PT_PATH=/opt/CAD/Synopsys/Current/pts/bin
